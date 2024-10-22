c068993bd84c31ff8711429f0581b55a62840605
cristian farfan <cfarfansepulveda@gmail.com>
2024-10-22 15:02:45 -0300

    add custom user models

--- project_realstate/estate-src/real_estate/urls.py
+++ project_realstate/estate-src/real_estate/urls.py
@@ -6,3 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Real Estate Administration"
+
+admin.site.site_title = "Real Estate Admin"
+
+admin.site.index_title = "Welcome to the Real Estate Admin Portal"
