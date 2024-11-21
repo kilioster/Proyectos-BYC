9d4cac93f736c858d0b3dede3227e3332d65b0de
cristian farfan <64711147+kilioster@users.noreply.github.com>
2024-11-21 08:55:13 -0300

    Delete project_realstate directory

--- project_realstate/estate-src/apps/profiles/exceptions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.exceptions import APIException
-
-class ProfileNotFound(APIException):
-    status_code = 404
-    default_detail = 'Profile not found.'
-    
-class NotYourProfile(APIException):
-    status_code = 403
-    default_detail = 'You cannot edit this profile.'
--- project_realstate/estate-src/real_estate/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate.settings.development')
-
-application = get_wsgi_application()
